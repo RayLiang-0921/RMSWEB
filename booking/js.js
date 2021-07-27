2f01aff96ae2399faf4118a61339b3a9ddff7381
RayLiang-0921 <80161113+RayLiang-0921@users.noreply.github.com>
2021-07-27 22:54:53 +0800

    add main

--- booking/js.js
+++ booking/js.js
@@ -6,6 +6,7 @@ to_top.addEventListener("click", function(){
     });
 }
 );
+const gallery = document.querySelector(".gallery");
 const navslide = ()=>{
     const NavBar = document.querySelector(".burger");
     const Navigation = document.querySelector(".pointer");
@@ -17,5 +18,39 @@ const navslide = ()=>{
     });
 };
 navslide();
+////////////////////////////////
+const GetData = async () => {
+    fetch('https://hotels-com-provider.p.rapidapi.com/v1/hotels/photos?hotel_id=363464', {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": apikey,
+            "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com"
+        }
+    })
+    .then(response =>(response.json()))
+    .then(response => {
+        console.log(response);
+        for(i = 0; i < response.length; i++) {
+            const image = response[i].mainUrl;
+            const cell = document.createElement("div");
+            cell.className = "cell";
+            gallery.appendChild(cell);
+            const HotelsName = document.createElement("a");
+            cell.appendChild(HotelsName);
+            const photo = document.createElement("img");
+            cell.appendChild(photo);
+            const PhoneNumber = document.createElement("div");
+            PhoneNumber.innerText="123"
+            cell.appendChild(PhoneNumber);
+            const Discription = document.createElement("div");
+            cell.appendChild(Discription);
 
-////////////////////////////////booking stuff.
+        };
+
+    })
+    .catch(err => {
+        console.error(err);
+    });
+};
+
+GetData();
